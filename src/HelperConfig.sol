676df4796707ac2f3762f6424e7f98ec1cb05ef7
arfanxn <arfan2173@gmail.com>
2025-02-19 08:00:03 +0700

    update config for mainnet and sepolia

--- src/HelperConfig.sol
+++ src/HelperConfig.sol
@@ -3,12 +3,6 @@ pragma solidity ^0.8.28;
 
 import {MultiOwnable} from "src/libraries/MultiOwnable.sol";
 
-/*
-TODO: creates HelperCofig
-    - add it in deployer, constructor and so on
-    - fix it later
-*/
-
 contract HelperConfig is MultiOwnable {
     error InvalidChainId();
 
@@ -53,17 +47,14 @@ contract HelperConfig is MultiOwnable {
      * @dev constructor
      */
     constructor() {
-        _initialize();
+        _initializeMultiOwnable();
 
         uint256 chainId = block.chainid;
 
-        if (chainId == MAINNET_CHAIN_ID) {
-            // TODO
-        } else if (chainId == LOCAL_CHAIN_ID) {
-            // TODO
-        } else {
-            revert InvalidChainId();
-        }
+        if (chainId == MAINNET_CHAIN_ID) _setMainnetConfig();
+        else if (chainId == SEPOLIA_CHAIN_ID) _setSepoliaConfig();
+        else if (chainId == LOCAL_CHAIN_ID) _setLocalConfig();
+        else revert InvalidChainId();
     }
 
     // function getMainnetConfig() public pure returns (NetworkConfig memory) {
@@ -131,13 +122,85 @@ contract HelperConfig is MultiOwnable {
     }
 
     //==============================================================
-    //                     Inline helper functions
+    //                     Utility functions
     //==============================================================
 
-    function _initialize() private {
+    function _initializeMultiOwnable() private {
         address[] memory _owners = new address[](1);
         _owners[0] = msg.sender;
         super.initialize(_owners);
+    }
+
+    /**
+     * @dev Sets the tokens and price feeds to their mainnet values.
+     * @notice This function is only called once when the contract is first deployed.
+     */
+    function _setMainnetConfig() private {
+        // set tokens
+        setToken(HelperConfig.Token.ETH, address(0));
+        setToken(
+            HelperConfig.Token.WETH,
+            0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+        );
+        setToken(
+            HelperConfig.Token.USDC,
+            0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+        );
+        setToken(
+            HelperConfig.Token.MKR,
+            0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
+        );
+        // set price feeds
+        setPriceFeed(
+            HelperConfig.Token.WETH,
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+        setPriceFeed(
+            HelperConfig.Token.ETH,
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+        setPriceFeed(
+            HelperConfig.Token.USDC,
+            0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6
+        );
+        setPriceFeed(
+            HelperConfig.Token.MKR,
+            0xec1D1B3b0443256cc3860e24a46F108e699484Aa
+        );
+    }
+
+    function _setSepoliaConfig() private {
+        // set tokens
+        setToken(HelperConfig.Token.ETH, address(0));
+        setToken(
+            HelperConfig.Token.WETH,
+            0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9
+        );
+        setToken(
+            HelperConfig.Token.USDC,
+            0xf08A50178dfcDe18524640EA6618a1f965821715
+        );
+        setToken(
+            HelperConfig.Token.MKR,
+            0x67E0256715Dbe4A72CDBf657D8866b8E6Ba4463E
+        );
+        // set price feeds
+        setPriceFeed(
+            HelperConfig.Token.WETH,
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        setPriceFeed(
+            HelperConfig.Token.ETH,
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        setPriceFeed(
+            HelperConfig.Token.USDC,
+            0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E
+        );
+    }
+
+    function _setLocalConfig() private {
+        // Do nothing
     }
 
     function getTokenEnumValues()
